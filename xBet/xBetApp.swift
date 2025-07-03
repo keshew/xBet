9149f732905ca6d27b5631bc5f7fb82c3df8ff34
keshew <artyom.korotckov2017@yandex.ru>
2025-07-03 22:11:52 +0500

    Initial Commit

---
+++ xBet/xBetApp.swift
@@ -0,0 +1,17 @@
+//
+//  xBetApp.swift
+//  xBet
+//
+//  Created by Артём Коротков on 03.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct xBetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
